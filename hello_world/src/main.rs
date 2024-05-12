07d94fcbb5044203965df38efa1f4bb5eab3d803
scab1001 <sc222ab@leeds.ac.uk>
2024-05-12 23:58:16 +0100

    Adding hello_world directory to the repository

---
+++ hello_world/src/main.rs
@@ -0,0 +1,72 @@
+// Reg comment
+
+/*
+* Block comment
+*/
+
+//! This is a documentation comment !//
+
+/// Documentation comment
+/// # Examples
+use std::fmt;
+
+fn main() {
+    // The `{}` will be automatically replaced w stringified args
+    println!("{} days", 31);
+
+    // Positional arguments can be used. 
+    // Arguments start at 0 immediately after the format string.
+    println!("{0}, this is {1}. {1}, this is {0}", "Alice", "Bob");
+    // => Alice, this is Bob. Bob, this is Alice
+
+    // As can named arguments.
+    // println!("{subject} {verb} {object}",
+    //          object="the lazy dog",
+    //          subject="the quick brown fox",
+    //          verb="jumps over");
+
+    // Different formatting can be invoked by specifying the format character
+    // println!("Base 10:               {}",   69420); // 69420
+    // println!("Base 2 (binary):       {:b}", 69420); // 10000111100101100
+    // println!("Base 8 (octal):        {:o}", 69420); // 207454
+    // println!("Base 16 (hexadecimal): {:x}", 69420); // 10f2c
+
+    // You can right-justify text with a specified width. This will
+    // output "    1". (Four white spaces and a "1", for a total width of 5.)
+    println!("{number:>5}", number=1);
+
+    // You can pad numbers with extra zeroes,
+    println!("{number:0<5}", number=1); // 10000
+
+    // You can use named arguments in the format specifier by appending a `$`.
+    println!("{number:0>width$}", number=1, width=5); // 00001
+
+    // Rust even checks to make sure the correct number of arguments are used.
+    println!("My name is {0}, {1} {0}", "Bond", "James");
+
+    // Only types that implement fmt::Display can be formatted with `{}`. User-
+    // defined types do not implement fmt::Display by default.
+
+    #[allow(dead_code)] // disable `dead_code` which warn against unused module
+    struct Structure(i32);
+
+    // This will not compile because `Structure` does not implement
+    // fmt::Display.
+    println!("This struct `{}` won't print...", Structure(3));
+
+    // To fix the above error, we must implement fmt::Display for `Structure`.
+    // This implementation is a simple example and would not be appropriate for
+    // a real use case.
+    impl fmt::Display for Structure {
+        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+            write!(f, "{}", self.0)
+        }
+    }
+
+    // For Rust 1.58 and above, you can directly capture the argument from a
+    // surrounding variable. Just like the above, this will output
+    // "    1", 4 white spaces and a "1".
+    let number: f64 = 1.0;
+    let width: usize = 5;
+    println!("{number:>width$}");
+} 
