c07ec04007026664c24c9c1aab62fa000b92c78f
scab1001 <sc222ab@leeds.ac.uk>
2024-05-13 11:00:38 +0100

    Implementing lists, some looping and how to display items.

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,23 +1,21 @@
-use std::fmt; // Import the `fmt` module.
+use std::fmt;
 
 // Define a structure named `List` containing a `Vec`.
 struct List(Vec<i32>);
 
 impl fmt::Display for List {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        // Extract the value using tuple indexing,
-        // and create a reference to `vec`.
+        // Extract the value using tuple indexing, and create a reference to `vec`.
         let vec = &self.0;
 
         write!(f, "[")?;
 
-        // Iterate over `v` in `vec` while enumerating the iteration
-        // count in `count`.
+        // Iterate over `v` in `vec` while enumerating the iteration count in `count`.
         for (count, v) in vec.iter().enumerate() {
             // For every element except the first, add a comma.
             // Use the ? operator to return on errors.
             if count != 0 { write!(f, ", ")?; }
-            write!(f, "{}", v)?;
+            write!(f, "{}: {}", count, v)?;
         }
 
         // Close the opened bracket and return a fmt::Result value.
