e1b9ee1b1ff23c16510e753fdb953628261a09a9
scab1001 <sc222ab@leeds.ac.uk>
2024-05-13 10:54:09 +0100

    Display and using Debug and std::fmt.

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,15 +1,77 @@
-#[derive(Debug)]
 #[allow(dead_code)] // disable `dead_code` which warn against unused module
-struct Person<'a> {
-    name: &'a str,
-    age: u8
+use std::fmt; // Import `fmt`
+
+// A structure holding two numbers. `Debug` will be derived so the results can
+// be contrasted with `Display`.
+// #[derive(Debug)]
+// struct MinMax(i64, i64);
+
+// // Implement `Display` for `MinMax`.
+// impl fmt::Display for MinMax {
+//     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+//         // Use `self.number` to refer to each positional data point.
+//         write!(f, "({}, {})", self.0, self.1)
+//     }
+// }
+
+// Define a structure where the fields are nameable for comparison.
+// #[derive(Debug)]
+// struct Point2D {
+//     x: f64,
+//     y: f64,
+// }
+
+// // Similarly, implement `Display` for `Point2D`.
+// impl fmt::Display for Point2D {
+//     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+//         // Customize so only `x` and `y` are denoted.
+//         write!(f, "x: {}, y: {}", self.x, self.y)
+//     }
+// }
+
+#[derive(Debug)]
+struct Complex {
+    real: f64,
+    imag: f64,
 }
 
-fn main() {
-    let name = "Peter";
-    let age = 27;
-    let peter = Person { name, age };
+// Similarly, implement `Display` for `Complex`.
+impl fmt::Display for Complex {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        // Customize so only `x` and `y` are denoted.
+        write!(f, "{} + {}i", self.real, self.imag)
+    }
+}
 
-    println!("\n\tStandard:\n{:?}", peter);
-    println!("\n\tPretty:\n{:#?}", peter);
+
+fn main() {
+    // let minmax = MinMax(0, 14);
+
+    // println!("Compare structures:");
+    // println!("Display: {}", minmax);
+    // println!("Debug: {:?}", minmax);
+
+    // let big_range =   MinMax(-300, 300);
+    // let small_range = MinMax(-3, 3);
+
+    // println!("The big range is {big} and the small is {small}",
+    //          small = small_range,
+    //          big = big_range);
+
+    // let point = Point2D { x: 3.3, y: 7.2 };
+
+    // println!("Compare points:");
+    // println!("Display: {}", point);
+    // println!("Debug: {:?}", point);
+
+
+    let complex = Complex { real: 3.3, imag: 7.2 };
+    println!("\nDisplay: {}", complex);
+    println!("Debug: {:?}\n", complex);
+
+
+
+    // Error. Both `Debug` and `Display` were implemented, but `{:b}`
+    // requires `fmt::Binary` to be implemented. This will not work.
+    // println!("What does Point2D look like in binary: {:b}?", point);
 }
